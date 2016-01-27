d9e02df3d4f15a1a8c9dbaa3aee41c415f384add
aguptaadco <abhishek@adcoprod.com>
2016-01-26 21:54:17 -0800

    Android changes

--- src/android/com/mbppower/CameraPreview.java
+++ src/android/com/mbppower/CameraPreview.java
@@ -212,6 +212,9 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
 
 	private boolean stopCamera(final JSONArray args, CallbackContext callbackContext) {
 		if(fragment == null){
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
 			return false;
 		}
 
@@ -220,7 +223,9 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
 		fragmentTransaction.remove(fragment);
 		fragmentTransaction.commit();
 		fragment = null;
-
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
 		return true;
 	}
 
